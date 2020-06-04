26174a5878942e71733d407e3d5c34de04c3aacf
AIRALPHA <allphaair@gmail.com>
2020-06-04 10:50:53 +0200

    End ui for reset email password

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -12,6 +12,6 @@ return [
     |
     */
 
-    'failed'   => 'Identifiants incorects',
+    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements',
     'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
 ];
