c52ae776baa582a05026b85b1464ea04f81d99f2
AIRALPHA <allphaair@gmail.com>
2020-06-20 12:58:25 +0200

    Change all commandes icon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -173,7 +173,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             </li>
                             <li class="nav-item">
                                 <router-link to="/all-orders" class="nav-link">
-                                    <i class="fas fa-comment-dots nav-icon"></i>
+                                    <i class="fab fa-first-order-alt nav-icon"></i>
                                     <p>All Commandes</p>
                                 </router-link>
                             </li>
